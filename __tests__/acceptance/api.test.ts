b2edddbf775acf27b7eb60724d0a50edc7fec156
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-05-01 23:43:55 -0600

    test: Add future tests descriptions

--- __tests__/acceptance/api.test.ts
+++ __tests__/acceptance/api.test.ts
@@ -3,27 +3,30 @@ import supertest from "supertest";
 import app from "../../src/app";
 
 describe("API tests", () => {
-  describe("when requesting /api/whoami", () => {
-    it("should have CORS enabled", async () => {
-      const response = await supertest(app).get("/api/whoami");
+  it("should have CORS enabled", async () => {
+    const response = await supertest(app).get("/api/whoami");
 
-      expect(Object.keys(response.headers)).toContain(
-        "access-control-allow-origin"
-      );
+    expect(Object.keys(response.headers)).toContain(
+      "access-control-allow-origin"
+    );
+  });
+
+  describe("when requesting /api/users", () => {
+    describe("when POST to /api/users", () => {
+      it.todo("should create a new user");
     });
 
-    it("should return JSON with headers information", async () => {
-      const response = await supertest(app).get("/");
-
-      expect(response.body).toMatchObject({
-        message: "Hello, world!",
-      });
+    describe("when GET to /api/users", () => {
+      it.todo("should return an array of users");
     });
+  });
 
-    it("should respond with status code 200", async () => {
-      const response = await supertest(app).get("/");
+  describe("when requesting /api/users/:_id/exercises", () => {
+    it.todo("should return user with exercise field");
+  });
 
-      expect(response.statusCode).toBe(200);
-    });
+  describe("when requesting /api/users/:_id/logs", () => {
+    it.todo("should return user with log array field with all exercises in it");
+    it.todo("should return log when filtering parameters are provided");
   });
 });
