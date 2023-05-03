16e0cbfae679973abaaf149fef3f9bfa0e0ed4ca
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-05-03 15:33:11 -0600

    refactor: Get User's log use case

--- src/use-cases/user/get-log.ts
+++ src/use-cases/user/get-log.ts
@@ -1,20 +1,20 @@
 import { userDb } from "../../data-access";
 import models from "../../models";
-import { IExercise } from "src/models/exercise";
+import { IExercise } from "../../models/exercise";
 
 export default async function (_id: string) {
   try {
     const user = await userDb.findOne({ _id }, { populate: ["log"] });
 
     let logs: Array<IExercise | undefined> = [];
-    if (user?.log && user.log.length) {
+    if (user?.log?.length) {
       logs = user.log.map((log) => {
         if (log instanceof models.Exercise) {
           return {
             description: log.description,
             duration: log.duration,
             date: log.date,
-          } as IExercise;
+          };
         }
       });
     }
