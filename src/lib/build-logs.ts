6b2146a7150531ab0d702c1098df89f34bc6206d
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-05-03 21:22:59 -0600

    fix: Logs endpoint response shape

--- src/lib/build-logs.ts
+++ src/lib/build-logs.ts
@@ -1,18 +1,23 @@
 import { HydratedDocument } from "mongoose";
 
 import models from "../models";
-import { IExercise } from "../models/exercise";
 import { IUser } from "../models/user";
 
+interface Log {
+  description: string;
+  duration: number;
+  date: string | undefined;
+}
+
 export default function (user: HydratedDocument<IUser>) {
-  let logs: Array<IExercise | undefined> = [];
+  let logs: Array<Log | undefined> = [];
   if (user?.log?.length) {
     logs = user.log.map((exercise) => {
       if (exercise instanceof models.Exercise) {
         return {
           description: exercise.description,
           duration: exercise.duration,
-          date: exercise.date,
+          date: exercise.date?.toDateString(),
         };
       }
     });
