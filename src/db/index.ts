d023b18f5eff379ae4b49e694d23ff28a7a5357a
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-05-03 21:23:12 -0600

    fix: MongoDB connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,7 @@
 import mongoose from "mongoose";
 
-const onConnected = () => {
-  if (process.env.NODE_ENV !== "test") {
-    console.log("MongoDB connected");
-  }
-};
-const onError = (error: Error) => console.error(error.message);
-
 const connectDB = async (mongoURL: string) => {
-  mongoose.connect(`${mongoURL}`).then((_) => console.log("DB Connected"));
+  mongoose.connect(`${mongoURL}`).then(() => console.log("DB Connected"));
 };
 
 export { connectDB as default };
